78fbd240c8ea5894e47ea2c72e6cb77eeeca37e2
sajjadafzal <sajjadafzal@gmail.com>
2019-08-26 15:22:45 +0500

    Rectangle Draw with mouse implemented

--- src/renderer/components/stage.ts
+++ src/renderer/components/stage.ts
@@ -1,10 +1,15 @@
 import Konva from 'konva'
-import { canvasState, CanvasModes } from './utilities'
+import { canvasState, CanvasModes, Point } from './utilities'
 import { KonvaEventObject } from 'konva/types/Node'
+import { start } from 'repl'
 
 var stage: Konva.Stage
+var startPos = new Point()
+var endPos = new Point()
+var inProgressRect: Konva.Rect | undefined
 
 export let parentLayer = new Konva.Layer()
+let workingLayer
 
 export function createCanvas() {
   stage = new Konva.Stage({
@@ -15,10 +20,67 @@ export function createCanvas() {
   //canvas.getContainer().style.border = '1px solid black'
   stage.add(parentLayer)
 
-  stage.on('mousedown', function(e) {
+  stage.on('mousedown', function(e: KonvaEventObject<MouseEvent>) {
     switch (canvasState.getMode()) {
       case CanvasModes.DRAW_RECTANGLE:
-        drawRectangle(e)
+        startPos.x = e.evt.clientX
+        startPos.y = e.evt.clientY
+        var obj = {
+          x: startPos.x,
+          y: startPos.y,
+          width: 0,
+          height: 0,
+          stroke: 'gray',
+          strokeWidth: canvasState.shapesStrokeWidth,
+          dash: [5, 5],
+          dashEnabled: true,
+        }
+
+        inProgressRect = new Konva.Rect(obj)
+        parentLayer.add(inProgressRect)
+        //drawRectangle(e)
+        break
+      case CanvasModes.SELECTION:
+      default:
+        return
+    }
+  })
+
+  stage.on('mouseup', function(e: KonvaEventObject<MouseEvent>) {
+    switch (canvasState.getMode()) {
+      case CanvasModes.DRAW_RECTANGLE:
+        if (!inProgressRect) return
+
+        endPos.x = e.evt.clientX
+        endPos.y = e.evt.clientY
+
+        inProgressRect.remove()
+        inProgressRect = undefined
+        parentLayer.draw()
+        let width = endPos.x - startPos.x
+        let height = endPos.y - startPos.y
+
+        if (width < 10 || height < 10) return
+        drawRectangle(startPos, endPos)
+        break
+      case CanvasModes.SELECTION:
+      default:
+        return
+    }
+  })
+
+  stage.on('mousemove', function(e: KonvaEventObject<MouseEvent>) {
+    switch (canvasState.getMode()) {
+      case CanvasModes.DRAW_RECTANGLE:
+        if (!inProgressRect) return
+
+        endPos.x = e.evt.clientX
+        endPos.y = e.evt.clientY
+
+        inProgressRect.setAttr('width', endPos.x - startPos.x)
+        inProgressRect.setAttr('height', endPos.y - startPos.y)
+        parentLayer.draw()
+        //drawRectangle(startPos, endPos)
         break
       case CanvasModes.SELECTION:
       default:
@@ -27,6 +89,18 @@ export function createCanvas() {
   })
 }
 
-function drawRectangle(e: KonvaEventObject<MouseEvent>) {
-  console.log(e)
+function drawRectangle(startPosition: Point, endPosition: Point) {
+  var rect = new Konva.Rect({
+    x: startPos.x,
+    y: startPos.y,
+    width: endPos.x - startPos.x,
+    height: endPos.y - startPos.y,
+    stroke: 'black',
+    strokeWidth: canvasState.shapesStrokeWidth,
+    draggable: false,
+  })
+
+  parentLayer.add(rect)
+  parentLayer.draw()
+  // console.log(e)
 }
--- src/renderer/components/utilities.ts
+++ src/renderer/components/utilities.ts
@@ -1,8 +1,17 @@
+/** SHAPES ENUMERATION */
 enum Shapes {
   RECTANTLE = 'RECTANGLE',
   TEXT = 'TEXT',
   OPTIONS = 'OPTIONS',
 }
+
+/** Point objects can store the x and y position of the stage or canvas*/
+export class Point {
+  x: number
+  y: number
+  constructor() {}
+}
+/** Different modes of canvas or stage */
 export enum CanvasModes {
   SELECTION = 'SELECTION',
   DRAW_RECTANGLE = 'DRAW_RECTANGLE',
@@ -17,8 +26,7 @@ class CanvasState {
 
   setDrawMode() {
     this.canvasMode = CanvasModes.DRAW_RECTANGLE
-
-    return this
+    return this /* For enabling syntax like "function1().function2()" */
   }
 
   setSelectionMode() {
@@ -29,6 +37,11 @@ class CanvasState {
   getMode(): CanvasModes {
     return this.canvasMode
   }
+
+  setMode(mode: CanvasModes) {
+    this.canvasMode = mode
+  }
 }
 
-export const canvasState = new CanvasState()
+class ChoiceBox()
+export const canvasState = new CanvasState() /* canvasState is an object reference and the reference will remain constant. Means we can change the object properties. */
